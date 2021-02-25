0e5f564ebe9834942f98be78fe76ed3094968428
Matías Irland <mirland@xmartlabs.com>
2021-02-25 11:29:01 -0300

    Upgrade versions

--- app/src/main/java/com/xmartlabs/swissknife/sample/data/SessionRepository.kt
+++ app/src/main/java/com/xmartlabs/swissknife/sample/data/SessionRepository.kt
@@ -1,7 +1,9 @@
 package com.xmartlabs.swissknife.sample.data
 
 import android.content.Context
-import androidx.datastore.preferences.createDataStore
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.xmartlabs.swissknife.datastore.DataStoreSource
 import com.xmartlabs.swissknife.sample.GsonDataStoreEntitySerializer
 import com.xmartlabs.swissknife.sample.data.model.Session
@@ -10,14 +12,16 @@ import kotlinx.coroutines.flow.Flow
 /**
  * Created by mirland on 02/10/20.
  */
+private const val DATA_STORE_NAME = "data_store_name"
+private val Context.appSessionDataStore: DataStore<Preferences> by preferencesDataStore(name = DATA_STORE_NAME)
+
 class SessionRepository(context: Context) {
   companion object {
-    private const val DATA_STORE_NAME = "data_store_name"
     private const val SESSION_REPOSITORY_KEY = "session"
   }
 
   private val dataStoreSource = DataStoreSource(
-      dataStore = context.createDataStore(name = DATA_STORE_NAME),
+      dataStore = context.appSessionDataStore,
       serializer = GsonDataStoreEntitySerializer()
   )
 
